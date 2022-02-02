3f0a8ead5b9e97489c242cce9984c1be04dff97e
Shota Shimazu <hornet.live.mf@gmail.com>
2022-02-03 03:00:55 +0900

    add code names

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python
 import os
 
+
+SIM_IDENTIFIERS = [
+    'Alpha',
+    'Bravo',
+    'Charlie',
+    'Delta',
+    'Echo',
+    'Foxtrot',
+    'Golf',
+    'Hotel',
+    'India',
+    'Juliet',
+    'Kilo',
+    'Lima',
+    'Mike',
+    'November',
+    'Oscar',
+    'Papa',
+    'Quebec',
+    'Romeo',
+    'Sierra',
+    'Tango',
+    'Uniform',
+    'Victor',
+    'Whisky',
+    'X-ray',
+    'Yankee',
+    'Zulu',
+]
+
 if __name__ == '__main__':
     print("Hello World!")
-    os.system('xcrun simctl create \'iPhone 13 (15.2) Node-A\' com.apple.CoreSimulator.SimDeviceType.iPhone-13 com.apple.CoreSimulator.SimRuntime.iOS-15-2')
+
+    for FONETIC in SIM_IDENTIFIERS:
+        os.system('xcrun simctl delete \'' + FONETIC + ' iPhone 13 (15.2)\' com.apple.CoreSimulator.SimDeviceType.iPhone-13 com.apple.CoreSimulator.SimRuntime.iOS-15-2')
