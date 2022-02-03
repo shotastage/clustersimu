b11fac302159283516fe4bdaae52a0fd938fa72f
Shota Shimazu <hornet.live.mf@gmail.com>
2022-02-03 20:36:37 +0900

    revise

--- main.py
+++ main.py
@@ -60,14 +60,40 @@ if __name__ == '__main__':
     if args[1] == 'clean':
         with open('created_sims.csv') as f:
             csvreader = csv.reader(f)
-            for row in csvreader:
+            for index, row in enumerate(csvreader):
+                if index == 0: continue
+
                 shell('xcrun simctl erase ' + row[0])
                 shell('xcrun simctl delete ' + row[0])
                 print('Erased ' + row[0] + ' and deleted.')
 
-    if args[1] == 'launch':
+    if args[1] == 'boot':
+        shell('open -a "Simulator"')
+
         with open('created_sims.csv') as f:
             csvreader = csv.reader(f)
-            for row in csvreader:
-                shell('xcrun simctl boot ' + row[0])
+            for index, row in enumerate(csvreader):
+                if index == 0: continue
+
+                res = shell('xcrun simctl boot ' + row[0])
+
+                print('Booted ' + row[0] + ' .')
+
+
+    if args[1] == 'shutdown':
+        with open('created_sims.csv') as f:
+            csvreader = csv.reader(f)
+            for index, row in enumerate(csvreader):
+                if index == 0: continue
+
+                shell('xcrun simctl shutdown ' + row[0])
+                print('Booted ' + row[0] + ' .')
+
+
+
+    if args[1] == 'test':
+        with open('created_sims.csv') as f:
+            csvreader = csv.reader(f)
+            for index, row in enumerate(csvreader):
+                if index == 0: continue
                 print('Booted ' + row[0] + ' .')
